9077fcb0afaeedb3489f20faca9a4495938307c3
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-24 22:30:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -140,6 +140,7 @@ function moveDodgerLeft() {
    */
   
   var left = positionToInteger(DODGER.style.left)
+  if(left <= 0) return;
   function move(){
     DODGER.style.left = `${left -= 4}px`
     
